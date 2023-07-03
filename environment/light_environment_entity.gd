6764fba77f1f50b5690b486c858f70cf07ed0a9d
Alexander Klingenbeck <alexander.klingenbeck@siemens-healthineers.com>
2023-07-03 14:03:29 +0200

    add env light

---
+++ environment/light_environment_entity.gd
@@ -0,0 +1,30 @@
+@tool
+class_name LightEnvironmentEntity
+extends QodotEntity
+
+var node: DirectionalLight3D = null
+
+func update_properties():
+	if not Engine.is_editor_hint():
+		return
+
+	for child in get_children():
+		remove_child(child)
+		child.queue_free()
+		
+	node = DirectionalLight3D.new()
+		
+	if &'pitch' in properties:
+		node.rotate(Vector3.RIGHT, deg_to_rad(properties[&'pitch']))
+		
+	if &'angle' in properties:
+		node.rotate(Vector3.UP, deg_to_rad(180 + properties[&"angle"]))
+	
+	add_child(node)
+	
+	if is_inside_tree():
+		var tree = get_tree()
+		if tree:
+			var edited_scene_root = tree.get_edited_scene_root()
+			if edited_scene_root:
+				node.set_owner(edited_scene_root)
