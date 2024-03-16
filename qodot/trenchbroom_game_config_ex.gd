4f5097d27c59126ebef1685cbff898dbb759c612
Alexander Klingenbeck <alexander.klingenbeck@gmx.de>
2024-03-17 00:28:58 +0100

    Fixed TrenchBroom paths

--- qodot/trenchbroom_game_config_ex.gd
+++ qodot/trenchbroom_game_config_ex.gd
@@ -6,6 +6,6 @@ func do_export_file():
 	if OS.get_name() == "Windows":
 		trenchbroom_games_folder = "%s/TrenchBroom/games"%OS.get_environment("APPDATA")
 	if OS.get_name() == "macOS":
-		trenchbroom_games_folder = "~/Library/Application Support/TrenchBroom/games"		
+		trenchbroom_games_folder = "%s/Library/Application Support/TrenchBroom/games"%OS.get_environment("HOME")	
 	print("Setting Trenchbroom games folder: %s"%trenchbroom_games_folder)
 	super()
